6a83742706721b0783ee887530d479d6155be6e8
pacor <pacor@YO-GABA-GABA>
2023-11-06 11:21:04 -0600

    Add project files.

---
+++ EncryptData_Initial.cpp
@@ -0,0 +1,102 @@
+// EncryptData.cpp
+//
+// This file uses the input data and key information to encrypt the input data
+//
+
+#include "Main.h"
+
+// YOU WILL DELETE ALL THIS CODE WHEN YOU DO YOUR PROJECT - THIS IS GIVING YOU EXAMPLES OF HOW TO 
+// ACCESS gPasswordHash AND gKey
+
+void encryptData_01(char *data, int datalength)
+{
+	int test[] = { 255,255,255,255,255 };
+
+	__asm
+	{
+		// Set up new stack frame
+		push esi
+		push edi
+		lea edi, [ebp-20h]
+		mov ecx, 8
+		mov eax, 0CCCCCCCCh
+		rep stos dword ptr es:[edi]
+
+		// Define a "Round" Variable
+		xor ecx, ecx
+		mov [ebp-12], ecx
+
+		// starting_index[round] = gPasswordHash[0+round*4] * 256 + gPasswordHash[1+round*4]
+		movzx eax, [gPasswordHash + ecx * 4]
+		shl eax, 8
+		movzx ebx, [gPasswordHash + 1 + ecx * 4]
+		add eax, ebx
+		mov [ebp-8], eax
+
+		// Iterate through each byte in data 
+		xor ecx, ecx
+
+		XOR_LOOP:
+			cmp ecx, datalength
+			jge DONE
+
+			inc ecx
+			jmp XOR_LOOP
+
+
+	DONE:
+		pop edi
+		pop esi
+	}
+
+	return;
+} // encryptData_01
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE code to to show how to access global variables
+int encryptData(char *data, int dataLength)
+{
+	int resulti = 0;
+
+	gdebug1 = 0;				// a couple of global variables that could be used for debugging
+	gdebug2 = 0;				// also can have a breakpoint in C code
+
+	// You can not declare any local variables in C, but should use resulti to indicate any errors
+	// Set up the stack frame and assign variables in assembly if you need to do so
+	// access the parameters BEFORE setting up your own stack frame
+	// Also, you cannot use a lot of global variables - work with registers
+
+	__asm {
+		// you will need to reference some of these global variables
+		// (gptrPasswordHash or gPasswordHash), (gptrKey or gkey), gNumRounds
+
+		// simple example that xors 2nd byte of data with 14th byte in the key file
+		lea esi,gkey				// put the ADDRESS of gkey into esi
+		mov esi,gptrKey;			// put the ADDRESS of gkey into esi (since *gptrKey = gkey)
+
+		lea	esi,gPasswordHash		// put ADDRESS of gPasswordHash into esi
+		mov esi,gptrPasswordHash	// put ADDRESS of gPasswordHash into esi (since unsigned char *gptrPasswordHash = gPasswordHash)
+
+		mov al,byte ptr [esi]				// get first byte of password hash
+		mov al,byte ptr [esi+4]				// get 5th byte of password hash
+		mov ebx,2
+		mov al,byte ptr [esi+ebx]			// get 3rd byte of password hash
+		mov al,byte ptr [esi+ebx*2]			// get 5th byte of password hash
+
+		mov ax,word ptr [esi+ebx*2]			// gets 5th and 6th bytes of password hash ( gPasswordHash[4] and gPasswordHash[5] ) into ax
+		mov eax,dword ptr [esi+ebx*2]		// gets 4 bytes, as in:  unsigned int X = *( (unsigned int*) &gPasswordHash[4] );
+
+		mov al,byte ptr [gkey+ebx]			// get's 3rd byte of gkey[] data
+
+		mov al,byte ptr [gptrKey+ebx]		// THIS IS INCORRECT - will add the address of the gptrKey global variable (NOT the value that gptrKey holds)
+
+		mov al,byte ptr [esi+0xd];			// access 14th byte in gkey[]: 0, 1, 2 ... d is the 14th byte
+		mov edi,data				// Put ADDRESS of first data element into edi
+		xor byte ptr [edi+1],al		// Exclusive-or the 2nd byte of data with the 14th element of the keyfile
+									// NOTE: Keyfile[14] = 0x21, that value changes the case of a letter and flips the LSB
+									// Capital "B" = 0x42 becomes lowercase "c" since 0x42 xor 0x21 = 0x63
+	}
+
+	return resulti;
+} // encryptData
+
---
+++ Main.h
@@ -0,0 +1,73 @@
+// Main.h
+//
+// This file is the main header file for the CS3843 Encrypt/Decrypt project
+//
+
+// Include Files
+#include <windows.h>
+#include <stdio.h>
+#include <io.h>
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define CRYPTO_ORDER "CBEAD\0"
+#define var_Index -4
+#define var_HopCnt -8
+#define var_RoundNum -12
+#define var_DataLength -16
+
+
+#define FALL_2017		201708
+#define SPRING_2018		201801
+#define SUMMER_2018		201806
+#define FALL_2018		201808
+#define SPRING_2019		201901
+#define SPRING_2020		SPRING_2019
+#define FALL_2020		202008
+#define FALL_2021		FALL_2020
+#define FALL_2022		202208
+#define SPRING_2023		202301
+#define FALL_2023		202308
+
+#define CURRENT_SEMESTER FALL_2023
+
+// Structures
+
+
+// Prototypes
+int sha256(char *fileName, char *dataBuffer, DWORD dataLength, unsigned char sha256sum[32]);
+
+BYTE runOperations(BYTE originalData, BYTE encryptFlag);
+
+void encryptData_01(char *data, int length);
+void decryptData_01(char *data, int length);
+void encryptData_02(char *data, int length);
+void decryptData_02(char *data, int length);
+int encryptData_03(char *data, int length);
+int decryptData_03(char *data, int length);
+
+
+// Global Variable Extern Declarations
+extern unsigned char gkey[65537];
+extern unsigned char *gptrKey;			// used for inline assembly routines, need to access this way for Visual Studio
+extern char gPassword[256];
+extern unsigned char gPasswordHash[32];
+extern unsigned char *gptrPasswordHash;			// used for inline assembly routines, need to access this way for Visual Studio
+
+extern char gCRYPTO_ORDER[8];
+extern unsigned char gdebug1, gdebug2;
+
+extern FILE *gfptrIn;
+extern FILE *gfptrOut;
+extern FILE *gfptrKey;
+extern char gInFileName[256];
+extern char gOutFileName[256];
+extern char gKeyFileName[256];
+extern int gOp;			// 1 = encrypt, 2 = decrypt
+extern int gNumRounds;
+
+extern unsigned char gEncodeTable[256];
+extern unsigned char gDecodeTable[256];
+
+
