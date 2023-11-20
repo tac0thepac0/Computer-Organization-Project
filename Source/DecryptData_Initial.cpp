b0a482e5030a8125fa8c00e890f088d0f195b6e4
tac0thepac0 <paco.rodriguez.vara@gmail.com>
2023-11-19 19:56:11 -0600

    Fixed instruction order

--- Source/DecryptData_Initial.cpp
+++ Source/DecryptData_Initial.cpp
@@ -38,33 +38,18 @@ void decryptData_01(char *data, int sized)
 			movzx al, [edi]
 			movzx bl, [edx]
 
-<<<<<<< HEAD
-			
-=======
+			// (#A) code table swap 0x43 -> CodeTable[0x43] == 0xC4
+			// (#B) nibble rotate out 0xC4 -> 0x92 abcd efgh -> bcda hefg
+			// (#C) reverse bit order 0x92 -> 0x49 abcd efgh -> hgfe dcba
 			// (#E) rotate 3 bits left 0xDC -> 0xE6 abcd efgh -> defg habc
 			ror al, 3
+
 			// (#D) invert bits 0,2,4,7 0x49 -> 0xDC abcd efgh -> XbcX dXbX
 			xor al, 169 //10101001
 
 			xor al, bl
->>>>>>> 2f1ce8571aa181c42a0830641373adef5cf6eca1
-			// (#A) code table swap 0x43 -> CodeTable[0x43] == 0xC4
-			// (#B) nibble rotate out 0xC4 -> 0x92 abcd efgh -> bcda hefg
-				rol al, 4
-			// (#C) reverse bit order 0x92 -> 0x49 abcd efgh -> hgfe dcba
-<<<<<<< HEAD
-			// (#E) rotate 3 bits left 0xDC -> 0xE6 abcd efgh -> defg habc
-				ror al, 3
-			// (#D) invert bits 0,2,4,7 0x49 -> 0xDC abcd efgh -> XbcX dXbX
-				xor al, 169 //10101001
-
-				xor al, bl
 
 			mov [edi], al
-=======
-			
-			mov[edi], al
->>>>>>> 2f1ce8571aa181c42a0830641373adef5cf6eca1
 			inc ecx							// Move to next character in buffer
 			inc edi
 			jmp XOR_LOOP
